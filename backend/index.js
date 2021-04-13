9e9b3a1bf0c6f4a624bd12f1e4ac91a92dbd8ee0
rik61072@gmail.com <rik61072@gmail.com>
2021-04-13 16:59:32 +0530

    basic api

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express();
+const port = 4000;
+//const redis = require("redis");
+
+//const redisPort = 6379;
+//const client = redis.createClient(redisPort);
+
+//var mongoose = require('mongoose');
+
+//var mongoDB = 'mongodb://127.0.0.1/saheli';
+
+//mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+app.use(express.json())
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+    );
+    res.header("Access-Control-Allow-Methods","*")
+    next();
+  });
+
+app.get('/', (req, res) => {
+  res.json({"saheli" : "saheli"})
+});
+
+app.listen(port, () => {
+  console.log(`saheli API should be up and running on ${port}`)
+});
